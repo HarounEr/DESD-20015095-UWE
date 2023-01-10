7949136afb1aa17436f42a016d1045ad6e6ea131
Haroun Aiouadj <77894031+HarounEr@users.noreply.github.com>
2023-01-10 18:09:35 +0000

    Delete account_project directory

--- account_project/hello/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ClubName, ClubRep#, Profile, MyModel
-# Register your models here.
-admin.site.register(ClubName)
-admin.site.register(ClubRep)
-# admin.site.register(Profile)
-# admin.site.register(MyModel)
